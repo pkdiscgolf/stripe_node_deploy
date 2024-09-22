014cfb53df4c3bb6dd4b203b52a6eff8e74817f8
unknown <kellypatrick1986@gmail.com>
2024-09-22 16:43:39 -0500

    redeploy

--- public/store.js
+++ public/store.js
@@ -6,7 +6,7 @@ if (document.readyState == 'loading') {
 
 function ready() {
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
+    for (var i = 0; i < removeCartItemButtons.length; i++) { 
         var button = removeCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
     }
