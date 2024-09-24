285b993e3bc8746c3f48bc4502f2795d56dd5f24
unknown <kellypatrick1986@gmail.com>
2024-09-24 15:21:22 -0500

    redeploy

--- api/index.js
+++ api/index.js
@@ -21,17 +21,18 @@ app.use(express.static('public'));
 
 app.get('/store', (req, res) => {
     console.log('store')
-    // fs.readFile('items.json', (error, data) => {
-    //     if(error){
-    //         res.status(500).end()
-    //     } else {
+    fs.readFile('items.json', (error, data) => {
+        if(error){
+            res.status(500).end()
+        } else {
     //         res.render('store.ejs', {
     //             stripePublicKey: stripePublicKey, 
     //             items: JSON.parse(data)
     //         });
-    //     }
-    // });
     res.send("Hello!")
+        }
+    });
+    
     
 });
 
