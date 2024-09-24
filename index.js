e14252ed02b0bfa1d4f1ebc69976a95d17733acb
unknown <kellypatrick1986@gmail.com>
2024-09-24 11:52:34 -0500

    add error page for 404 errors

--- index.js
+++ index.js
@@ -65,6 +65,10 @@ app.post('/purchase', (req, res) => {
 
 });
 
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404! Page not found</h1>');
+  });
+
 app.listen(3000)
 
 module.exports = app;
--- server.js
+++ server.js
@@ -62,4 +62,8 @@ app.post('/purchase', (req, res) => {
 
 });
 
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404! Page not found</h1>');
+  });
+
 app.listen(3000)
